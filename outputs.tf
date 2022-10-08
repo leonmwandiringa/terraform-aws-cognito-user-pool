a29cd1ba666b4f9444b9e0c5727ef78d26098acd
Leon Mwandringa <leonmwandiringa@gmail.com>
2022-10-08 11:36:53 +0200

    pool client output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
-output "id" {
+output "user_pool_id" {
   value = aws_cognito_user_pool.default.id
   description = "cognito pool id"
 }
 
-output "arn" {
+output "user_pool_arn" {
   value = aws_cognito_user_pool.default.arn
   description = "cognito pool arn"
+}
+
+output "pool_client_id" {
+  value = join("", aws_cognito_user_pool_client.default.*.id)
+  description = "cognito pool client id"
+}
+
+output "pool_client_secret" {
+  value = join("", aws_cognito_user_pool_client.default.*.id)
+  description = "cognito pool client id"
 }
